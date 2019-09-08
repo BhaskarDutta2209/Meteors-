187935c74a5f15fd2eb2c96752461091571cd16a
BhaskarDutta2209 <bhaskardutta2209@gmail.com>
2019-09-09 02:57:16 +0530

    explosion effects added

--- meteors.py
+++ meteors.py
@@ -5,9 +5,11 @@ import random
 import time
 from os import path
 
+#loading directories
 img_dir = path.join(path.dirname(__file__),"img")
 snd_dir = path.join(path.dirname(__file__),"snd")
 
+#defining game constants
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
@@ -23,6 +25,41 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
+
+#load game graphics
+
+background = pygame.image.load(path.join(img_dir,"background.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir,"playerShip.png")).convert()
+laser_img = pygame.image.load(path.join(img_dir,"laserRed01.png")).convert()
+meteor_images = []
+meteor_list = ["meteor.png","meteorBrown_big1.png","meteorBrown_big2.png","meteorBrown_big3.png","meteorBrown_big4.png",
+               "meteorBrown_med1.png","meteorBrown_med3.png","meteorBrown_small1.png","meteorBrown_small2.png",
+               "meteorBrown_tiny1.png","meteorBrown_tiny2.png"]
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(path.join(img_dir,img)).convert())
+
+explosion_anim = {}
+explosion_anim["sm"] = []
+explosion_anim["lg"] = []
+
+for i in range(9):
+	filename = "regularExplosion0{}.png".format(i)
+	img = pygame.image.load(path.join(img_dir,filename)).convert()
+	img.set_colorkey(BLACK)
+	img_lg = pygame.transform.scale(img,(75,75))
+	img_sm = pygame.transform.scale(img,(32,32))
+	explosion_anim["sm"].append(img_sm)
+	explosion_anim["lg"].append(img_lg)
+
+#load game audio
+
+shoot_sound = pygame.mixer.Sound(path.join(snd_dir,"pew.wav"))
+ship_blast = pygame.mixer.Sound(path.join(snd_dir,"shipBlast.wav"))
+exp_sounds = [pygame.mixer.Sound(path.join(snd_dir,"meteorBlast1.wav")),pygame.mixer.Sound(path.join(snd_dir,"meteorBlast2.wav"))]
+pygame.mixer.music.load(path.join(snd_dir,"backgroundMusic.ogg"))
+pygame.mixer.music.set_volume(0.4)
+
 
 font_name = pygame.font.match_font("arial")
 
@@ -50,6 +87,30 @@ def newMob():
     m = Mob()
     mobs.add(m)
     all_sprites.add(m)
+
+class Explosion(pygame.sprite.Sprite):
+	def __init__(self, center, size):
+		pygame.sprite.Sprite.__init__(self)
+		self.size = size
+		self.image = explosion_anim[self.size][0]
+		self.rect = self.image.get_rect()
+		self.rect.center = center
+		self.frame = 0
+		self.last_update = pygame.time.get_ticks()
+		self.frame_rate = 50
+
+	def update(self):
+		now = pygame.time.get_ticks()
+		if now - self.last_update > self.frame_rate:
+			self.last_update = now
+			self.frame += 1
+			if self.frame == len(explosion_anim[self.size]):
+				self.kill()
+			else:
+				center = self.rect.center
+				self.image = explosion_anim[self.size][self.frame]
+				self.rect = self.image.get_rect()
+				self.rect.center = center 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -146,27 +207,7 @@ class Mob(pygame.sprite.Sprite):
             self.speedx = random.randrange(-5,5)
             self.speedy = random.randrange(1,10)
 
-#load game graphics
 
-background = pygame.image.load(path.join(img_dir,"background.png")).convert()
-background_rect = background.get_rect()
-player_img = pygame.image.load(path.join(img_dir,"playerShip.png")).convert()
-laser_img = pygame.image.load(path.join(img_dir,"laserRed01.png")).convert()
-meteor_images = []
-meteor_list = ["meteor.png","meteorBrown_big1.png","meteorBrown_big2.png","meteorBrown_big3.png","meteorBrown_big4.png",
-               "meteorBrown_med1.png","meteorBrown_med3.png","meteorBrown_small1.png","meteorBrown_small2.png",
-               "meteorBrown_tiny1.png","meteorBrown_tiny2.png"]
-
-#load game audio
-
-shoot_sound = pygame.mixer.Sound(path.join(snd_dir,"pew.wav"))
-ship_blast = pygame.mixer.Sound(path.join(snd_dir,"shipBlast.wav"))
-exp_sounds = [pygame.mixer.Sound(path.join(snd_dir,"meteorBlast1.wav")),pygame.mixer.Sound(path.join(snd_dir,"meteorBlast2.wav"))]
-pygame.mixer.music.load(path.join(snd_dir,"backgroundMusic.ogg"))
-pygame.mixer.music.set_volume(0.4)
-
-for img in meteor_list:
-    meteor_images.append(pygame.image.load(path.join(img_dir,img)).convert())
 
 all_sprites = pygame.sprite.Group()
 player = Player()
@@ -201,6 +242,8 @@ while running:
     for hit in hits:
         random.choice(exp_sounds).play()
         score += int(50 - hit.radius)
+        expl = Explosion(hit.rect.center,"lg")
+        all_sprites.add(expl)
         newMob()
 
     #check for collision with block
@@ -209,6 +252,8 @@ while running:
         newMob()
         player.sheild -= hit.radius * 2
         ship_blast.play()
+        expl = Explosion(hit.rect.center,"sm")
+        all_sprites.add(expl)
         if player.sheild <= 0:
             running = False
 
